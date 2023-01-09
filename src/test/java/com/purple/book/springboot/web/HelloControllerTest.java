f8065d0c49312f3760cc74cbc4ac33a3fff9cfff
miaaaking <jinie128@gmail.com>
2023-01-09 23:22:56 +0900

    DTO 생성

--- src/test/java/com/purple/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/purple/book/springboot/web/HelloControllerTest.java
@@ -8,9 +8,9 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@RunWith(SpringRunner.class) //junit ver4
 // JUnit 내장 실행자 + 스프링 실행자 SpringExtension 연결 수행
@@ -34,4 +34,18 @@ public class HelloControllerTest {
                 .andExpect(status().isOk()) // HTTP Header의 Status(200인지 ok인지) 검증
                 .andExpect(content().string(hello)); // perform 결과 검증(Controller에서 응답하는 hello가 맞는지 검증)
     }
+
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        String name = "hello";
+        int amount = 1000;
+
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
+    }
+
 }
